741bce4ee6d4e28bc22188d59ff67f1549a20418
Youennpds <71871891+Youennpds@users.noreply.github.com>
2020-12-17 18:33:03 +0100

    Update docFinal.hs

--- docFinal.hs
+++ docFinal.hs
@@ -31,7 +31,7 @@ add (x:xs) out s = case x of
     -- on ajoute au début de la liste des instructions i fois les instructions extraites du repeat
         where rep = (take ((length j) * i) (cycle j)) ++ xs
 
--- fonction qui retourne une liste de traits au format xml/svg au format liste de String
+-- fonction qui retourne la liste de traits au format xml/svg au format liste de String
 -- Cette fonction prends en entrée la liste des deplacements du crayon restants à effectuer, la position courante du crayon (cx,cy), et la liste des traits déjà ecrits au format SVG
 lgSvg :: [(Float,Float)] -> (Float,Float) -> [[Char]] -> [[Char]]
 lgSvg [] _ _ = []
@@ -47,12 +47,12 @@ decomplist [] a = a
 decomplist (x:xs) s = 
     s ++ x ++ decomplist xs s 
 
--- fonction qui ajoute l'en-tete et le pied de page au fichier pour respecter la syntaxe xml à la sting contenant l'ensemble des traits
+-- fonction qui rajoute l'en tête et le bas de page du code xlm
 buildfile :: [Char] -> String
 buildfile a = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"500\" height=\"500\">\n<title>Exemple</title>\n"++a++"</svg>"        
 
 main = do
-     --Recuperation des instructions logoskell dans une variable
+     --Recuperation des instructions logoskell du fichier txt
      texte <- readFile "logoskell.txt"
      
      -- composition de toutes les précédentes fonctions 
